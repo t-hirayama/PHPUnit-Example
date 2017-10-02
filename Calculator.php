a020279e919e04bac9d7879e2fd8902cf32add7d
Takaaki Hirayama <dev.takaaki.hirayama@gmail.com>
2017-10-03 00:28:38 +0900

    テストのサンプルを作成

---
+++ Calculator.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hirayamatakaaki
+ * Date: 2017/10/03
+ * Time: 0:14
+ */
+class Calculator
+{
+    public function add($a, $b)
+	{
+		return $a + $b;
+	}
+
+	public function sub($a, $b)
+	{
+        return $a - $b;
+	}
+
+	public function mul($a, $b)
+	{
+        return $a * $b;
+	}
+
+	public function div($a, $b)
+	{
+        return $a / $b;
+	}
+}
+
---
+++ MyTestCase.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hirayamatakaaki
+ * Date: 2017/10/03
+ * Time: 0:18
+ */
+require_once("Calculator.php");
+
+class MyTestCase extends PHPUnit_Framework_TestCase
+{
+    public function test_add()
+    {
+        $calc = new Calculator();
+        $this->assertEquals( 8, $calc->add( 6, 2));
+    }
+
+    public function test_sub()
+    {
+        $calc = new Calculator();
+        $this->assertEquals( 4, $calc->sub( 6, 2));
+    }
+
+    public function test_mul()
+    {
+        $calc = new Calculator();
+        $this->assertEquals( 12, $calc->mul( 6, 2));
+    }
+
+    public function test_div()
+    {
+        $calc = new Calculator();
+        $this->assertEquals( 3, $calc->div( 6, 2));
+    }
+}
